989697a030f16157270c816c3a76406919b3725c
steven <seokju73@gmail.com>
2022-04-01 21:22:29 +0900

    feat: Add repository interface in program management section

---
+++ src/main/java/com/gymmer/gymmerstation/programManagement/ProgramRepository.java
@@ -0,0 +1,15 @@
+package com.gymmer.gymmerstation.programManagement;
+
+import com.gymmer.gymmerstation.domain.Program;
+
+import java.util.List;
+
+public interface ProgramRepository {
+    void addProgram(Program program);
+
+    List<String> showProgramList();
+
+    void editProgram(Program program);
+
+    void deleteProgram(Program program);
+}
