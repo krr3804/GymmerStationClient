0a1bd8aecac8a833adfac51415273c3b9c5480e6
steven <seokju73@gmail.com>
2022-04-01 13:05:28 +0900

    feat: Add member variable in 'Exercise' object

--- src/main/java/com/gymmer/gymmerstation/domain/Exercise.java
+++ src/main/java/com/gymmer/gymmerstation/domain/Exercise.java
@@ -6,12 +6,14 @@ public class Exercise {
     String name;
     Long set;
     Long rep;
+    Long weight;
     SimpleDateFormat rest = new SimpleDateFormat("mm:ss");
 
-    public Exercise(String name, Long set, Long rep, SimpleDateFormat rest) {
+    public Exercise(String name, Long set, Long rep, Long weight, SimpleDateFormat rest) {
         this.name = name;
         this.set = set;
         this.rep = rep;
         this.rest = rest;
+        this.weight = weight;
     }
 }
