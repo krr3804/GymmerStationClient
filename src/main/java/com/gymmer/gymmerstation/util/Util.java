3f95fd69fb619fc10018911773754a872382818b
steven <seokju73@gmail.com>
2022-04-02 21:40:43 +0900

    refactor: alter window switching manner

---
+++ src/main/java/com/gymmer/gymmerstation/util/Util.java
@@ -0,0 +1,27 @@
+package com.gymmer.gymmerstation.util;
+
+import com.gymmer.gymmerstation.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Util {
+    public static void loadStage(String fxml, Scene scene) {
+        try {
+            Parent root = FXMLLoader.load(Main.class.getResource(fxml));
+            Stage stage =  (Stage) scene.getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void closeStage(Button btn) {
+        Stage stage = (Stage) btn.getScene().getWindow();
+        stage.close();
+    }
+}
