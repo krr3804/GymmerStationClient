808f9e88171f0dc5b4266acfbaf667f517eec749
steven <seokju73@gmail.com>
2022-04-12 01:27:28 +0900

    feat: add program operation serivice interface and its implementation

---
+++ src/main/java/com/gymmer/gymmerstation/programOperation/ProgramOperationService.java
@@ -0,0 +1,14 @@
+package com.gymmer.gymmerstation.programOperation;
+
+import com.gymmer.gymmerstation.domain.Exercise;
+import com.gymmer.gymmerstation.domain.OperationDataExercise;
+import com.gymmer.gymmerstation.domain.OperationDataProgram;
+import com.gymmer.gymmerstation.domain.Program;
+
+import java.util.List;
+
+public interface ProgramOperationService {
+    void saveProgramData(Program program, List<OperationDataProgram> odpList);
+
+    void deleteProgramData(Program program);
+}
---
+++ src/main/java/com/gymmer/gymmerstation/programOperation/ProgramOperationServiceImpl.java
@@ -0,0 +1,21 @@
+package com.gymmer.gymmerstation.programOperation;
+
+import com.gymmer.gymmerstation.domain.Exercise;
+import com.gymmer.gymmerstation.domain.OperationDataExercise;
+import com.gymmer.gymmerstation.domain.OperationDataProgram;
+import com.gymmer.gymmerstation.domain.Program;
+
+import java.util.List;
+
+public class ProgramOperationServiceImpl implements ProgramOperationService{
+
+    @Override
+    public void saveProgramData(Program program, List<OperationDataProgram> odpList) {
+
+    }
+
+    @Override
+    public void deleteProgramData(Program program) {
+
+    }
+}
