a227983150826eeb0027989e02432fb3d15743d0
steven <seokju73@gmail.com>
2022-04-22 00:27:34 +0900

    feat: add object 'Program' as a variable in OperationDataProgram

--- src/main/java/com/gymmer/gymmerstation/domain/OperationDataProgram.java
+++ src/main/java/com/gymmer/gymmerstation/domain/OperationDataProgram.java
@@ -4,14 +4,20 @@ import java.util.List;
 import java.util.Map;
 
 public class OperationDataProgram {
-    int week;
-    int division;
-    List<OperationDataExercise> odExerciseList;
+    private Program program;
+    private int week;
+    private int division;
+    private List<OperationDataExercise> odExerciseList;
 
-    public OperationDataProgram(int week, int division, List<OperationDataExercise> odExerciseList) {
+    public OperationDataProgram(Program program, int week, int division, List<OperationDataExercise> odExerciseList) {
+        this.program = program;
         this.week = week;
         this.division = division;
         this.odExerciseList = odExerciseList;
+    }
+
+    public Program getProgram() {
+        return program;
     }
 
     public int getWeek() {
