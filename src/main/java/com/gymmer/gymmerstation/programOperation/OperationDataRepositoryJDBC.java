943f8143d55d05776124485dd18f00178934e9f4
steven <seokju73@gmail.com>
2022-04-22 03:53:15 +0900

    feat: add program operation data returning method in JDBC repository

--- src/main/java/com/gymmer/gymmerstation/programOperation/OperationDataRepositoryJDBC.java
+++ src/main/java/com/gymmer/gymmerstation/programOperation/OperationDataRepositoryJDBC.java
@@ -5,10 +5,8 @@ import com.gymmer.gymmerstation.domain.OperationDataExercise;
 import com.gymmer.gymmerstation.domain.OperationDataProgram;
 import com.gymmer.gymmerstation.domain.Program;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OperationDataRepositoryJDBC implements OperationDataRepository {
@@ -22,15 +20,19 @@ public class OperationDataRepositoryJDBC implements OperationDataRepository {
         Long division = dataProgram.getDivision();
 
         try {
-            String query = "INSERT INTO performanceData VALUES (?,?,?,?,?,?)";
+            String query = "INSERT INTO performanceData VALUES (?,?,?,?,?,?,?,?,?,?)";
             psmt = conn.prepareStatement(query);
             for(OperationDataExercise dataExercise : dataProgram.getOdExerciseList()) {
                 psmt.setLong(1,week);
                 psmt.setLong(2,division);
                 psmt.setString(3,dataExercise.getTimeConsumed());
                 psmt.setString(4,dataExercise.getName());
-                psmt.setLong(5,dataExercise.getDivision());
-                psmt.setLong(6,program_id);
+                psmt.setLong(5,dataExercise.getSet());
+                psmt.setLong(6,dataExercise.getRep());
+                psmt.setLong(7,dataExercise.getWeight());
+                psmt.setString(8,dataExercise.getRestTime());
+                psmt.setLong(9,dataExercise.getDivision());
+                psmt.setLong(10,program_id);
                 psmt.addBatch();
                 psmt.clearParameters();
             }
@@ -62,7 +64,55 @@ public class OperationDataRepositoryJDBC implements OperationDataRepository {
 
     @Override
     public List<OperationDataProgram> getDataListByProgram(Program program) {
-        return null;
+        Connection conn = getConnection();
+        PreparedStatement psmt = null;
+        ResultSet rs = null;
+        List<OperationDataProgram> list = new ArrayList<>();
+        try {
+            String query = "SELECT * FROM performanceData WHERE program = ?";
+            psmt = conn.prepareStatement(query);
+            psmt.setLong(1,program.getId());
+            rs = psmt.executeQuery();
+            OperationDataProgram currentDataProgram = null;
+            while (rs.next()) {
+                Long week = rs.getLong("week");
+                Long division = rs.getLong("division");
+                if (currentDataProgram == null) {
+                    mapOperationDataProgram(program,week,division,rs);
+                } else if(!currentDataProgram.getWeek().equals(week) || !currentDataProgram.getDivision().equals(division)) {
+                    list.add(currentDataProgram);
+                    currentDataProgram = mapOperationDataProgram(program,week,division,rs);
+                }
+                currentDataProgram.getOdExerciseList().add(mapOperationDataExercise(rs));
+            }
+            if (currentDataProgram != null) {
+                list.add(currentDataProgram);
+            }
+            rs.close();
+
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            closePreparedStatement(psmt);
+            closeConnection(conn);
+        }
+        return list;
+    }
+
+    private OperationDataProgram mapOperationDataProgram(Program program, Long week, Long division, ResultSet rs) throws SQLException {
+        OperationDataProgram dataProgram = new OperationDataProgram(
+                program, week, division, new ArrayList<>()
+        );
+        return dataProgram;
+    }
+
+    private OperationDataExercise mapOperationDataExercise(ResultSet rs) throws SQLException {
+        OperationDataExercise dataExercise = new OperationDataExercise(
+                rs.getString("exercise_name"), rs.getLong("exercise_sets"), rs.getLong("exercise_reps"),
+                rs.getLong("exercise_weight"), rs.getString("exercise_rest"), rs.getLong("exercise_division"),
+                rs.getString("timeConsumed")
+        );
+        return dataExercise;
     }
 
     @Override
