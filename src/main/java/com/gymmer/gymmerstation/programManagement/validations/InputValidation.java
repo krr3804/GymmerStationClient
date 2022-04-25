8b74d2c7f7f4d3994591d0a409fc14052f661935
steven <seokju73@gmail.com>
2022-04-26 04:05:22 +0900

    feat: add validation conditions for input in prgraom management section

---
+++ src/main/java/com/gymmer/gymmerstation/programManagement/validations/InputValidation.java
@@ -0,0 +1,45 @@
+package com.gymmer.gymmerstation.programManagement.validations;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class InputValidation {
+    private static final String INPUT_BLANK = " Is Blank!";
+    private static final String INPUT_MISMATCH = " Must Be Numeric!";
+    private static final String NEGATIVE_DIGIT = " Must Be No Less Than 1!";
+    private static final String REST_TIME = "Rest Time Is 00:00!";
+
+    public static void inputBlankValidation(LinkedHashMap<String, String> map) {
+        StringBuilder sb = new StringBuilder();
+        for(Map.Entry entry : map.entrySet()) {
+            if(entry.getValue().toString().isBlank()) {
+                sb.append(entry.getKey()).append(", ");
+            }
+        }
+        if(!sb.toString().isBlank()) {
+            sb.delete(sb.length()-2,sb.length());
+            sb.append(INPUT_BLANK);
+            throw new IllegalArgumentException(sb.toString());
+        }
+    }
+
+    public static void inputMismatchValidationNumber(String input, String field) {
+        Long num;
+        try {
+            num = Long.parseLong(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(field + INPUT_MISMATCH);
+        }
+
+        if(num < 1) {
+            throw new IllegalArgumentException(field + NEGATIVE_DIGIT);
+        }
+    }
+
+    public static void inputMismatchValidationRestTime(String input) {
+        if(input.equals("00:00")) {
+            throw new IllegalArgumentException(REST_TIME);
+        }
+    }
+}
