efc5d911c75a8ec696a995a91be34ff830d41334
steven <seokju73@gmail.com>
2022-04-25 18:35:01 +0900

    feat: add alert generating methods in Util

--- src/main/java/com/gymmer/gymmerstation/util/Util.java
+++ src/main/java/com/gymmer/gymmerstation/util/Util.java
@@ -4,8 +4,12 @@ import com.gymmer.gymmerstation.Main;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
+
+import java.util.Optional;
 
 public class Util {
     public static void loadStage(String fxml, Scene scene) {
@@ -22,5 +26,21 @@ public class Util {
     public static void closeStage(Button btn) {
         Stage stage = (Stage) btn.getScene().getWindow();
         stage.close();
+    }
+
+    public static Alert generateErrorAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("ERROR MESSAGE");
+        alert.setHeaderText("ERROR!");
+        alert.setContentText(message);
+        return alert;
+    }
+
+    public static Alert generateConfirmationAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("CONFIRMATION");
+        alert.setHeaderText("ARE YOU SURE?");
+        alert.setContentText(message);
+        return alert;
     }
 }
