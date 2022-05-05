ef1d88da6cb0ff4160e24259667e329ae0d927b1
steven <seokju73@gmail.com>
2022-05-05 14:26:20 +0900

    feat: add drag and drop function for program data termination

--- src/main/java/com/gymmer/gymmerstation/performanceArchive/PerformanceArchiveListController.java
+++ src/main/java/com/gymmer/gymmerstation/performanceArchive/PerformanceArchiveListController.java
@@ -3,9 +3,7 @@ package com.gymmer.gymmerstation.performanceArchive;
 import com.gymmer.gymmerstation.AppConfig;
 import com.gymmer.gymmerstation.Main;
 import com.gymmer.gymmerstation.domain.Program;
-import com.gymmer.gymmerstation.programManagement.ProgramService;
 import com.gymmer.gymmerstation.programOperation.ProgramOperationService;
-import com.gymmer.gymmerstation.util.Util;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,13 +11,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.ListView;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.*;
+import javafx.scene.input.*;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 import java.net.URL;
 import java.util.Optional;
@@ -27,7 +22,6 @@ import java.util.ResourceBundle;
 
 import static com.gymmer.gymmerstation.util.CommonValidation.noIndexSelectedValidation;
 import static com.gymmer.gymmerstation.util.Util.*;
-import static javafx.collections.FXCollections.observableList;
 
 public class PerformanceArchiveListController implements Initializable {
     private final ProgramOperationService programOperationService = AppConfig.programOperationService();
@@ -47,26 +41,111 @@ public class PerformanceArchiveListController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setListView();
-        programListInProgress.setOnMouseClicked(event -> handleListDoubleClickEvent(event));
-        btnView.setOnAction(event -> handleBtnViewAction(event));
-        btnDelete.setOnAction(event -> handleBtnDeleteAction(event));
+        formListDragDetectedAction(programListInProgress);
+        formListDragDroppedAction(programListTerminated);
+        btnView.setOnAction(event -> handleButtonEvents(event));
+        btnDelete.setOnAction(event -> handleButtonEvents(event));
         btnReturn.setOnAction(event -> handleBtnReturnAction(event));
     }
 
-    private void handleListDoubleClickEvent(MouseEvent event) {
-        if(event.getButton().equals(MouseButton.PRIMARY) && event.getClickCount() == 2) {
-            selectedItemIndex  = programListInProgress.getSelectionModel().getSelectedIndex();
-            Program program = programOperationService.getProgramByIndex(selectedItemIndex,false);
-            Optional<ButtonType> result = generateTerminateProgramAlert(program.getName()).showAndWait();
-            if(result.get() == ButtonType.YES) {
-                programOperationService.terminateProgram(program);
-                setListView();
+    private void handleButtonEvents(ActionEvent event) {
+        try {
+            if(event.getSource().equals(btnView)) {
+                handleBtnViewAction(event);
             }
+            if (event.getSource().equals(btnDelete)) {
+                handleBtnDeleteAction(event);
+            }
+        } catch (IllegalArgumentException e) {
+            generateErrorAlert(e.getMessage()).showAndWait();
+        }
+    }
+
+    private void formListDragDetectedAction(ListView<String> list) {
+        list.setCellFactory(new Callback<ListView<String>, ListCell<String>>() {
+            @Override
+            public ListCell<String> call(ListView<String> param) {
+                ListCell<String> listCell = new ListCell<String>() {
+                    @Override
+                    protected void updateItem(String item, boolean empty) {
+                        super.updateItem(item, empty);
+                        setText(item);
+                    }
+                };
+                listCell.setOnDragDetected((MouseEvent event) -> {
+                    selectedItemIndex = programListInProgress.getSelectionModel().getSelectedIndex();
+                    Dragboard db = listCell.startDragAndDrop(TransferMode.MOVE);
+                    ClipboardContent content = new ClipboardContent();
+                    content.putString(listCell.getItem());
+                    db.setContent(content);
+                    if(programListTerminated.getItems().isEmpty()) {
+                        programListTerminated.getItems().add("");
+                    }
+                });
+
+                listCell.setOnDragDone((DragEvent event) -> {
+                    if(programListTerminated.getItems().get(0).equals("")) {
+                        setListView();
+                    }
+                    selectedItemIndex = -1;
+                });
+
+                return listCell;
+            }
+        });
+    }
+    private void formListDragDroppedAction(ListView<String> list) {
+        list.setCellFactory(new Callback<ListView<String>, ListCell<String>>() {
+            @Override
+            public ListCell<String> call(ListView<String> param) {
+                ListCell<String> listCell = new ListCell<String>() {
+                    @Override
+                    protected void updateItem(String item, boolean empty) {
+                        super.updateItem(item, empty);
+                        setText(item);
+                    }
+                };
+
+                listCell.setOnDragEntered(event -> listCell.setStyle( "-fx-background-color: grey;" ));
+
+                listCell.setOnDragExited(event -> listCell.setStyle(""));
+
+                listCell.setOnDragOver( ( DragEvent event ) ->
+                {
+                    Dragboard db = event.getDragboard();
+                    if ( db.hasString() )
+                    {
+                        event.acceptTransferModes( TransferMode.MOVE );
+                    }
+                } );
+
+                listCell.setOnDragDropped(event -> {
+                    Dragboard db = event.getDragboard();
+                    if (db.hasString()) {
+                        terminateProgram(event);
+                        event.setDropCompleted(true);
+                    } else {
+                        event.setDropCompleted(false);
+                    }
+                    selectedItemIndex = -1;
+                });
+                return listCell;
+            }
+        });
+    }
+
+    private void terminateProgram(DragEvent event) {
+        Program program = programOperationService.getProgramByIndex(selectedItemIndex,false);
+        Optional<ButtonType> result = generateTerminateProgramAlert(program.getName()).showAndWait();
+        if(result.get() == ButtonType.YES) {
+            programOperationService.terminateProgram(program);
+            setListView();
         }
     }
 
     private void handleBtnViewAction(ActionEvent event) {
         checkSelectedList();
+        noIndexSelectedValidation(selectedItemIndex);
         try {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(Main.class.getResource("performance-archive-view.fxml"));
@@ -76,7 +155,6 @@ public class PerformanceArchiveListController implements Initializable {
             Stage stage = (Stage) btnView.getScene().getWindow();
             stage.setScene(new Scene(root));
             stage.show();
-            selectedItemIndex = -1;
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -102,11 +180,10 @@ public class PerformanceArchiveListController implements Initializable {
         checkSelectedList();
         noIndexSelectedValidation(selectedItemIndex);
         Program program = programOperationService.getProgramByIndex(selectedItemIndex,status);
-        Alert alert = generateDeleteDataAlert("Data");
+        Alert alert = generateDeleteDataAlert("");
         Optional<ButtonType> result = alert.showAndWait();
         if (result.get() == ButtonType.OK) {
             programOperationService.deleteProgramData(program,status);
-            selectedItemIndex = -1;
             setListView();
             generateInformationAlert("Data Deleted!").showAndWait();
         } else {
