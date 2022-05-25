e3a84a8da0247f4b8eea39e7ed83eff45c711916
krr3804 <seokju73@gmail.com>
2022-05-25 19:02:52 +0900

    refactor : apply singleton pattern to 'AppConfig' class

--- src/main/java/com/gymmer/gymmerstation/AppConfig.java
+++ src/main/java/com/gymmer/gymmerstation/AppConfig.java
@@ -4,12 +4,14 @@ import com.gymmer.gymmerstation.programManagement.*;
 import com.gymmer.gymmerstation.programOperation.*;
 
 public class AppConfig {
+    private static final ProgramService programService = new ProgramServiceImpl();
+    private static final ProgramOperationService programOperationService = new ProgramOperationServiceImpl();
     public static ProgramService programService() {
-        return new ProgramServiceImpl();
+        return programService;
     }
 
     public static ProgramOperationService programOperationService() {
-        return new ProgramOperationServiceImpl();
+        return programOperationService;
     }
 
 }
